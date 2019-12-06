cd4337e4ed08991d2bfa2780c9d761d25f5704d6
nicole <nbush89@gmail.com>
2019-12-05 22:13:01 -0500

    FINAL version of js file

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ var groupSize = prompt("How many people are in your group?");
 
 if (vacationType === "musical") {
     if (groupSize <= 2) {
-        let result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + ", you should go to New Orleans on First Class. Go in January to experience Mardi Gras, don't forget the beads!";
+        let result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + ", you should go to New Orleans on First Class. It's still warm this time of year, bring shorts and good walking shoes.";
         alert(result);
     }
     else if (groupSize >= 3 && groupSize <= 5) {
-        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go to New Orleans by helicopter. Go in January to experience Mardi Gras, don't forget the beads!";
+        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go to New Orleans by helicopter. It's still warm this time of year, bring shorts and good walking shoes.";
         alert(result);
     }
     else if (groupSize >= 6) {
-        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go to New Orleans and charter a flight there. Go in January to experience Mardi Gras, don't forget the beads!";
+        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go to New Orleans and charter a flight there. It's still warm this time of year, bring shorts and good walking shoes.";
         alert(result);
     }
 }
 else if (vacationType === "tropical") {
     if (groupSize <= 2) {
-        let result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + ", you should go on a beach vacation to Mexico on First Class. It's still hot this time of year, and don't forget to bring the sunscreen!";
+        let result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + ", you should go on a beach vacation to Mexico on First Class. It's still hot this time of year, don't forget to bring the sunscreen!";
         alert(result);
     }
     else if (groupSize >= 3 && groupSize <= 5) {
-        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go on a beach vacation to Mexico taking a helicopter there. It's still hot this time of year, and don't forget to bring the sunscreen!";
+        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go on a beach vacation to Mexico taking a helicopter there. It's still hot this time of year, don't forget to bring the sunscreen!";
         alert(result);
     }
     else if (groupSize >= 6) {
-        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go on a beach vacation to Mexico and charter a flight there. It's still hot this time of year, and don't forget to bring the sunscreen!";
+        result = "Since you're going on a " + vacationType + " vacation with a group of " + groupSize + " people, you should go on a beach vacation to Mexico and charter a flight there. It's still hot this time of year, don't forget to bring the sunscreen!";
         alert(result);
     }
 }
